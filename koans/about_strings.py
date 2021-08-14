0475887dd928ad46092aeeda629ea137258fd1e2
Rene Capella <renecapella.dev@gmail.com>
2021-08-14 12:32:23 -0700

    Python Koans Day 2 5%

--- koans/about_strings.py
+++ koans/about_strings.py
@@ -41,19 +41,18 @@ class AboutStrings(Koan):
     def test_use_backslash_at_the_end_of_a_line_to_continue_onto_the_next_line(self):
         string = "It was the best of times,\n\
 It was the worst of times."
-        self.assertEqual(__, len(string))
+        self.assertEqual(52, len(string))
 
     def test_triple_quoted_strings_can_span_lines(self):
-        string = """
-Howdy,
+        string = """Howdy,
 world!
 """
-        self.assertEqual(__, len(string))
+        self.assertEqual(14, len(string))
 
     def test_triple_quoted_strings_need_less_escaping(self):
         a = "Hello \"world\"."
         b = """Hello "world"."""
-        self.assertEqual(__, (a == b))
+        self.assertEqual(True, (a == b))
 
     def test_escaping_quotes_at_the_end_of_triple_quoted_string(self):
         string = """Hello "world\""""
